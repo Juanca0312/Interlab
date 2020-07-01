042592f0c3bcab3fa6f45bd9f8ac2eb1c752718d
Rodrigo Ramírez <8rb.academic@gmail.com>
2020-07-01 17:25:03 -0500

    Fixed register company and refactored code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,18 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
-import {DashboardStudentComponent} from "./dashboard-student/dashboard-student.component";
-import {RegisterComponent} from "./register/register.component";
-import {LoginComponent} from "./login/login.component";
-import {EditprofilestudentComponent} from "./editprofilestudent/editprofilestudent.component";
-import {BuscarpasantiaComponent} from "./buscarpasantia/buscarpasantia.component";
-import {CreateInternshipComponent} from "./create-internship/create-internship.component";
+import {DashboardStudentComponent} from './dashboard-student/dashboard-student.component';
+import {LoginComponent} from './login/login.component';
+import {EditprofilestudentComponent} from './editprofilestudent/editprofilestudent.component';
+import {BuscarpasantiaComponent} from './buscarpasantia/buscarpasantia.component';
+import {CreateInternshipComponent} from './create-internship/create-internship.component';
 
 const routes: Routes = [
-  { path: 'login', component:LoginComponent},
-  { path: 'dashboard-student', component:DashboardStudentComponent},
-  { path: 'register', component: RegisterComponent},
-  {path: 'editprofile', component: EditprofilestudentComponent},
-  {path: 'buscarpasantia', component: BuscarpasantiaComponent},
-  {path: 'create-internship', component: CreateInternshipComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'dashboardStudent', component: DashboardStudentComponent},
+  { path: 'editProfile', component: EditprofilestudentComponent},
+  { path: 'findInternship', component: BuscarpasantiaComponent},
+  { path: 'createInternship', component: CreateInternshipComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {NgForm} from "@angular/forms";
-import {MatTableDataSource} from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
 import axios from 'axios';
-import { AxiosInstance } from "axios";
+import {AxiosInstance} from 'axios';
 
 @Component({
   selector: 'app-login',
@@ -13,15 +10,15 @@ import { AxiosInstance } from "axios";
 export class LoginComponent implements OnInit {
   private axiosClient: AxiosInstance;
   hide1 = true;
-  username = "";
-  password = "";
+  username = '';
+  password = '';
   userId = null;
   auth = false;
   constructor() {
     this.axiosClient = axios.create({
       timeout: 3000,
       headers: {
-        "X-Initialized-At": Date.now().toString()
+        'X-Initialized-At': Date.now().toString()
       }
     });
   }
@@ -30,15 +27,16 @@ export class LoginComponent implements OnInit {
   }
 
   getUsers() {
-    console.log("BUTTON WORKING");
+    console.log('BUTTON WORKING');
     console.log(this.username);
     console.log(this.password);
     axios.get('https://interlab.azurewebsites.net/api/users')
       .then(response => {
         console.log(response.data.content);
+        // tslint:disable-next-line:prefer-for-of
         for (let i = 0; i < response.data.content.length; i++) {
             console.log(response.data.content[i].username);
-          if (response.data.content[i].username === this.username &&
+            if (response.data.content[i].username === this.username &&
             response.data.content[i].password === this.password ||
             response.data.content[i].email === this.username &&
             response.data.content[i].password === this.password) {
@@ -47,27 +45,27 @@ export class LoginComponent implements OnInit {
             break;
           }
         }
-        if(this.auth){
+        if (this.auth){
           console.log(this.userId);
           axios.get('https://interlab.azurewebsites.net/api/users/' + this.userId + '/profiles')
-            .then(response2=>{
+            .then(response2 => {
               console.log(response2.data.content[0].role);
-              if(response2.data.content[0].role == "student"){
-                alert("Welcome Student");
-                //router push student dashboard
+              if (response2.data.content[0].role === 'student'){
+                alert('Welcome Student');
+                // router push student dashboard
               }
-              else if(response2.data.content[0].role == "company"){
-                alert("Welcome Company");
-                //router push company dashboard
+              else if (response2.data.content[0].role === 'company'){
+                alert('Welcome Company');
+                // push company dashboard
               }
               else{
-                alert("Role not found");
+                alert('Role not found');
               }
             });
 
         }
-        if(!this.auth){
-          alert("Please register");
+        if (!this.auth){
+          alert('Please register');
         }
       });
   }
