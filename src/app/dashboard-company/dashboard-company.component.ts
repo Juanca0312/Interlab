6ac6906391f6ca5f48be6acf266ead368ca957a2
Juanca0312 <39997847+Juanca0312@users.noreply.github.com>
2020-07-03 14:37:19 -0500

    Listo el local storage en company

--- src/app/dashboard-company/dashboard-company.component.ts
+++ src/app/dashboard-company/dashboard-company.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 import {MediaMatcher} from '@angular/cdk/layout';
-import {CreateInternshipComponent} from "../create-internship/create-internship.component";
+import {CreateInternshipComponent} from '../create-internship/create-internship.component';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 
 @Component({
@@ -10,9 +11,9 @@ import {CreateInternshipComponent} from "../create-internship/create-internship.
 })
 export class DashboardCompanyComponent implements OnDestroy {
 
-  declarations: [CreateInternshipComponent]
+  declarations: [CreateInternshipComponent];
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -29,21 +30,29 @@ export class DashboardCompanyComponent implements OnDestroy {
   private _mobileQueryListener: () => void;
 
   mostrar(activo) {
-    if(activo === 'Home') {
+    if (activo === 'Home') {
+      console.log(localStorage.getItem('userId'));
       this.showHome = true;
       this.showInternship = false;
       this.showCreateInternship = false;
     }
     if (activo === 'Internship') {
+      console.log(localStorage.getItem('userId'));
       this.showHome = false;
       this.showInternship = true;
       this.showCreateInternship = false;
     }
     if (activo === 'CreateInternship') {
+      console.log(localStorage.getItem('userId'));
       this.showHome = false;
       this.showInternship = false;
       this.showCreateInternship = true;
     }
+  }
+
+  Logout() {
+    console.log('salir');
+    this.router.navigateByUrl('login');
   }
 
   ngOnDestroy(): void {
