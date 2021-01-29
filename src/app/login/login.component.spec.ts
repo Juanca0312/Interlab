8b1a915debd6b6ea8f07d692504067c95d2d6721
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-01-29 10:58:06 -0500

    ⚡ Finished Login Component
    Finished New Responsive Login Component

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { Login } from './login.component';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  let component: Login;
+  let fixture: ComponentFixture<Login>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ Login ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,88 +1,36 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import axios from 'axios';
-import {AxiosInstance} from 'axios';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  private axiosClient: AxiosInstance;
+export class Login implements OnInit {
   hide1 = true;
-  username = '';
-  name = '';
-  password = '';
+  username = 'student@gmail.com';
+  name = 'Jhon Student';
+  password = 'demopassword';
   companyId = null;
   userId = null;
-  auth = false;
+
+  public loginForm: FormGroup = new FormGroup({
+    username: new FormControl('student@gmail.com'),
+    password: new FormControl('demopassword'),
+  });
+
   constructor(private router: Router) {
-    this.axiosClient = axios.create({
-      timeout: 3000,
-      headers: {
-        'X-Initialized-At': Date.now().toString()
-      }
-    });
+
   }
   ngOnInit(): void {
 
   }
 
-  getUsers() {
-    console.log('BUTTON WORKING');
-    console.log(this.username);
-    console.log(this.password);
-    axios.get('https://interlab.azurewebsites.net/api/users')
-      .then(response => {
-        console.log(response.data.content);
-        // tslint:disable-next-line:prefer-for-of
-        for (let i = 0; i < response.data.content.length; i++) {
-            console.log(response.data.content[i].username);
-            if (response.data.content[i].username === this.username &&
-            response.data.content[i].password === this.password ||
-            response.data.content[i].email === this.username &&
-            response.data.content[i].password === this.password) {
-            this.userId = response.data.content[i].id;
-            this.auth = true;
-            break;
-          }
-        }
-        if (this.auth){
-          console.log(this.userId);
-          axios.get('https://interlab.azurewebsites.net/api/users/' + this.userId + '/profiles')
-            .then(response2 => {
-              console.log(response2.data.content[0].role);
-              if (response2.data.content[0].role === 'student'){
-                alert('Welcome Student');
-                this.name = response2.data.content[0].firstName;
-                localStorage.setItem('firstName', this.name);
-                localStorage.setItem('userId', this.userId);
-                this.router.navigateByUrl('/dashboardStudent');
-              }
-              else if (response2.data.content[0].role === 'company'){
-                axios.get('https://interlab.azurewebsites.net/api/users/' + this.userId + '/companies')
-                  .then(response3 => {
-                    this.name = response2.data.content[0].firstName;
-                    localStorage.setItem('firstName', this.name);
-                    this.companyId = response3.data.content[0].id;
-                    console.log(this.companyId);
-                    alert('Welcome Company');
-                    localStorage.setItem('userId', this.userId);
-                    localStorage.setItem('companyId', this.companyId);
-                    this.router.navigateByUrl('/dashboardCompany');
-                  });
-              }
-              else{
-                alert('Role not found');
-              }
-            });
-
-        }
-        if (!this.auth){
-          alert('Please register');
-        }
-      });
+  login() {
+    // localStorage.setItem('firstName', this.name);
+    this.router.navigateByUrl('/dashboardStudent');
+    //this.router.navigateByUrl('/dashboardCompany');
   }
   getUsername(event: any) { // without type info
     this.username = event.target.value;
