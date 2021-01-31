cbd0d7784cdd7a46e7f81ca03e07aa00ce396ad5
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-01-30 21:01:22 -0500

    ⚡️ Started Dashboard
    Started Dashboard Component.

--- src/app/dashboard-student/dashboard-student.component.ts
+++ src/app/dashboard-student/dashboard-student.component.ts
@@ -1,13 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatPaginator} from '@angular/material/paginator';
-import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef} from '@angular/core';
-import {EditprofilestudentComponent} from '../editprofilestudent/editprofilestudent.component';
-import {MyRequestsComponent} from '../my-requests/my-requests.component';
-import {FindInternshipComponent} from '../find-internship/find-internship.component';
-import {HomeStudentComponent} from '../home-student/home-student.component';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard-student',
@@ -15,72 +6,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard-student.component.css']
 })
 export class DashboardStudentComponent implements OnInit {
-  checked = false;
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
-  fillerContent = Array.from({length: 50}, () =>
-    `no repitas`);
+  showDropdown = false;
+  constructor(private router: Router) {}
 
-  showFiller = false;
-  @ViewChild('dashboard-student', {static: false} )
-  dashboardStudentForm: NgForm;
-  dataSource = new MatTableDataSource();
-  displayedColumns: string[] = ['id', 'state', 'description', 'publicationDate', 'startingDate', 'finishingDate', 'salary'];
+  ngOnInit(): void {}
 
-  @ViewChild(MatPaginator, {static: true})
-  paginator: MatPaginator;
-
-  isEditMode = false;
-
-  declarations: [FindInternshipComponent, EditprofilestudentComponent, MyRequestsComponent, HomeStudentComponent];
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-    this.showHome = true;
-    this.showMyRequests = false;
-    this.showEditProfile = false;
-    this.showFindInternship = false;
-    this.localStorage = localStorage.getItem('firstName');
-  }
-  mobileQuery: MediaQueryList;
-  showHome: boolean;
-  showMyRequests: boolean;
-  showEditProfile: boolean;
-  showFindInternship: boolean;
-  localStorage: string;
-  // tslint:disable-next-line:variable-name
-  private _mobileQueryListener: () => void;
-
-  mostrar(activo) {
-    if (activo === 'Dashboard') {
-      this.showHome = true;
-      this.showMyRequests = false;
-      this.showEditProfile = false;
-      this.showFindInternship = false;
-    }
-    if (activo === 'MyRequests') {
-      this.showHome = false;
-      this.showMyRequests = true;
-      this.showEditProfile = false;
-      this.showFindInternship = false;
-    }
-    if (activo === 'Edit_profile') {
-      this.showHome = false;
-      this.showMyRequests = false;
-      this.showEditProfile = true;
-      this.showFindInternship = false;
-    }
-    if (activo === 'FindInternship') {
-      this.showHome = false;
-      this.showMyRequests = false;
-      this.showEditProfile = false;
-      this.showFindInternship = true;
-    }
-  }
-  logout() {
-    this.router.navigateByUrl('/login');
-  }
-  ngOnInit(): void {
-    this.dataSource.paginator = this.paginator;
+  toggleDropdown(){
+    this.showDropdown = !this.showDropdown;
   }
 }
